366c9fde282a63eb720f6f25d6bf7d938a6c9cdb
Maxim Doronin <doro156@me.com>
2015-12-10 23:25:57 +0300

    Add throws. Exclude double entry of one variable

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -19,28 +19,43 @@ string postfix_notation(string expression)
 
 	for (int i = 0; i < expression.length(); i++){
 		buff = expression[i];
-		if (op.count(buff)) {																//обработка символов операций
-			if ((!opStack.isEmpty()) && (op[buff] < op[opStack.look()]) && (buff != '('))		//обработка операций 
-				while ((!opStack.isEmpty()) && (op[buff] <= op[opStack.look()]))				//с низким приоритетом
-					trackStack.push(opStack.pop());
-			
+		if (op.count(buff)) {			//обработка символов операций
+			if ((!opStack.isEmpty()) && (op[buff] <= op[opStack.look()]) && (buff != '('))
+				while ((!opStack.isEmpty()) && (op[buff] <= op[opStack.look()]))
+					trackStack.push(opStack.pop());	
+					//имеет ли новая операция меньший приоритет, по сравнению с вершиной стека?
+					//если да, то все операции с приоритетом <= приоритету текущей
+					//попадают в результирующий стек trackStack.
+					//операция '(' не сравнивается с вершиной стека, и попадает в него
+
 			opStack.push(buff);
+			continue;
 		}
 
-		if   (((buff >= 0x41)&&(buff <= 0x5A))
-			||((buff >= 0x61)&&(buff <= 0x7A))
-			||((buff >= 0x30)&&(buff <= 0x39)))				//обработка операндов
+		if   (((buff >= 0x41)&&(buff <= 0x5A))							//операнды попадают в 
+			||((buff >= 0x61)&&(buff <= 0x7A)))	{						//результирующий стек
 			trackStack.push(buff);
-		
-		if (buff == ')') {																	//обработка закрывающей
-			while(opStack.look() != '(')													//скобки
-				trackStack.push(opStack.pop());
-			opStack.pop();
+			continue;
 		}
-
+		
+		if (buff == ')') {												//операция "закрывающая скобка"									
+			while ((!opStack.isEmpty())&&(opStack.look() != '('))		//побуждает к извлечению операций из стека 									
+				trackStack.push(opStack.pop());							//и добавлению их к результрующему, до тех пор,
+			if (opStack.isEmpty())										//пока не встретится "открывающая скобка"
+				throw "brackets are not consistent";				
+			opStack.pop();
+			continue;
+		}
+		throw "invalid characters";
 	}
-	while(!opStack.isEmpty())
+	while(!opStack.isEmpty()){
+		if (opStack.look() == '(')
+			throw "brackets are not consistent";
 		trackStack.push(opStack.pop());
+	}
+
+	if (trackStack.isEmpty())
+		throw "no data";
 	
 	string result;
 	string tmp;
@@ -54,24 +69,33 @@ string postfix_notation(string expression)
 
 float postfix_calculation(string expression)
 {
+	if (expression == "")
+		throw "no data";
 	Stack<float> trackStack;
 	char buff;
-	float buffVal;
 	float leftOperand;
 	float rightOperand;
+
+	map<char, float> values;
 
 	cout << "Input values: " <<endl;
 	for (int i = 0; i < expression.length(); i++){
 		buff = expression[i];
 		if   (((buff >= 0x41)&&(buff <= 0x5A))
 			||((buff >= 0x61)&&(buff <= 0x7A))){
-			cout << buff << " = ";
-			cin  >> buffVal;
-			trackStack.push(buffVal);
+			if (!values.count(buff)){
+				cout << buff << " = ";
+				cin  >> values[buff];
+			}
+			trackStack.push(values[buff]);
 			continue;
 		}
 
+		if(trackStack.isEmpty()) 
+			throw "does not match the number of operands";
 		rightOperand = trackStack.pop();
+		if(trackStack.isEmpty()) 
+			throw "does not match the number of operands";
 		leftOperand  = trackStack.pop();
 
 		switch (buff){
@@ -80,7 +104,10 @@ float postfix_calculation(string expression)
 		case '*':{trackStack.push(leftOperand * rightOperand); break;}
 		case '/':{trackStack.push(leftOperand / rightOperand); break;}
 		}
-
 	}
-	return trackStack.look();
+
+	float result = trackStack.pop();
+	if(!trackStack.isEmpty())
+		throw "many operands";
+	return result;
 }
